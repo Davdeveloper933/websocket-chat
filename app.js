bbd21a5d1cf3af879e50d406de0dddb71f2fa364
unknown <daveistomin@gmail.com>
2023-05-15 01:53:33 +0400

    c

--- app.js
+++ app.js
@@ -18,15 +18,15 @@ app.use('/client',express.static(__dirname+'/client'));
 io.on('connection', socket => {
     var $liveIpAddress = socket.handshake.address;
       //check socket io online users
-  if (!$ipsConnected.hasOwnProperty($liveIpAddress)) {
-    $ipsConnected[$liveIpAddress] = 1;
-    flags++;
-            //socket io real time online users example
-            // socket.emit('socket_io_counter', flags);
- }
+//   if (!$ipsConnected.hasOwnProperty($liveIpAddress)) {
+//     $ipsConnected[$liveIpAddress] = 1;
+//     flags++;
+//             //socket io real time online users example
+//             // socket.emit('socket_io_counter', flags);
+//  }
  console.log("Good Luck, client is connected");
- socket.emit('socket_io_counter', flags);
- console.log(flags)
+ socket.emit('socket_io_counter', $liveIpAddress);
+ console.log($liveIpAddress)
     
     socket.emit('message', 'Привет, народ!')
     // io.emit('message', 'Привет, народ!') //Всем подключенным клиентам
@@ -41,11 +41,11 @@ io.on('connection', socket => {
     });
 
       /* Your Live (socket.io client flags) Disconnect socket */
-  socket.on('disconnect', function() {
-    if ($ipsConnected.hasOwnProperty($liveIpAddress)) {
-       delete $ipsConnected[$liveIpAddress];
-      flags--;
-      socket.emit('socket_io_counter', flags);
-    }
- });
+//   socket.on('disconnect', function() {
+//     if ($ipsConnected.hasOwnProperty($liveIpAddress)) {
+//        delete $ipsConnected[$liveIpAddress];
+//       flags--;
+//       socket.emit('socket_io_counter', flags);
+//     }
+//  });
 })
